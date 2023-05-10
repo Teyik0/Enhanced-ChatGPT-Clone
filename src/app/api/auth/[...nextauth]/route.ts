5071f424c15d2747306d90a7a88bc889d5d3dd50
Teyik0 <teyik0.fr@icloud.com>
2023-05-10 16:06:49 +0200

    test deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      checks: ['none']
     }),
   ],
 };
